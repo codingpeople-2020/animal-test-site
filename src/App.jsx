97947fec794ed92fa8f003e924478f9377d9f6e9
codingpeople-2020 <copeo2020@gmail.com>
2025-07-14 17:02:39 +0900

    Fix: import Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/Home';
+import Home from './pages/Home';
 import QuizPage from './pages/QuizPage';
 import ResultPage from './pages/ResultPage';
 
